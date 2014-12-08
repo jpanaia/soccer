4245b4d4441e4185da3d9867afcd7f41746d2b25
Jaime <jpanaia@gmail.com>
2014-12-08 17:36:04 -0500

    more updates

--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -1,6 +1,6 @@
 class Quiz < ActiveRecord::Base
 
-	validates :question, :corret_answer, :article_id, presence: true
+	#validates :question, :corret_answer, :article_id, presence: true
 
 	belongs_to :article
 end
